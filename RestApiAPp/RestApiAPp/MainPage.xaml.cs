3fd3607c19310a422828c5841de7b7d29478ebeb
webmasterdevlin <webmasterdevlin@gmail.com>
2017-12-08 16:11:03 +0800

    Add project files.

---
+++ RestApiAPp/RestApiAPp/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using Xamarin.Forms;
+
+namespace RestApiAPp
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "http://jsonplaceholder.typicode.com/posts";
+        private readonly HttpClient _client = new HttpClient();
+        private ObservableCollection<Post> _posts;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            string content = await _client.GetStringAsync(Url);
+            List<Post> posts = JsonConvert.DeserializeObject<List<Post>>(content);
+            _posts = new ObservableCollection<Post>(posts);
+            MyListView.ItemsSource = _posts;
+            base.OnAppearing();
+        }
+
+        private async void OnAdd(object sender, EventArgs e)
+        {
+            Post post = new Post { Title = $"Title: Timestamp {DateTime.UtcNow.Ticks}" };
+            string content = JsonConvert.SerializeObject(post);
+            _posts.Insert(0, post);
+            await _client.PostAsync(Url, new StringContent(content));
+        }
+
+        private async void OnUpdate(object sender, EventArgs e)
+        {
+            Post post = _posts[0];
+            post.Title += " [updated]";
+            string content = JsonConvert.SerializeObject(post);
+            await _client.PutAsync(Url + "/" + post.Id, new StringContent(content));
+        }
+
+        private async void OnDelete(object sender, EventArgs e)
+        {
+            Post post = _posts[0];
+            await _client.DeleteAsync(Url + "/" + post.Id);
+            _posts.Remove(post);
+        }
+    }
+}
---
+++ RestApiAPp/RestApiAPp/Post.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using RestApiAPp.Annotations;
+
+namespace RestApiAPp
+{
+    internal class Post : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+
+        private string _title;
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Body { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
